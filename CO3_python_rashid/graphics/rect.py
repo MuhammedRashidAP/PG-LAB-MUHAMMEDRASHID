590e8daa4ab652b6cd2cc3102e84538b2d335a78
MuhammedRashidAP <93854829+MuhammedRashidAP@users.noreply.github.com>
2021-12-20 20:20:04 +0530

    Delete CO3_python_rashid directory

--- CO3_python_rashid/graphics/rect.py
+++
@@ -1,3 +0,0 @@
-def arear(l,b):
-     print("area of rectangle is ",l*b)
-     print("perimeter of rectangle is",2*(l+b))
--- CO3_python_rashid/graphicspack1.py
+++
@@ -1,5 +0,0 @@
-from graphics import rect
-from graphics import circle
-rect.arear(int(input("enter length of rectangle")),int(input("enter breadth of rectangle")))
-circle.areac(int(input("enter radius of circle")))
-
