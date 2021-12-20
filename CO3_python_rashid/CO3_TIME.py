beb48b2d8721b48102c5240fa8b12e529e223105
MuhammedRashidAP <93854829+MuhammedRashidAP@users.noreply.github.com>
2021-12-20 21:57:24 +0530

    Add files via upload

---
+++ CO3_python_rashid/CO3_TIME.py
@@ -0,0 +1,16 @@
+import time
+print("current time in sec:",time.time())
+print("current time:",time.ctime())
+print("current time after 30 sec:",time.ctime(time.time()+30))
+t=time.localtime()
+print("time:",t)
+print("current Year:",t.tm_year)
+print("current Month:",t.tm_mon)
+print("current Day:",t.tm_mday)
+print("current Hour:",t.tm_hour)
+print("current week:",t.tm_wday)
+print("Day of Year:",t.tm_yday) 
+print("formatted time:",time.strftime("%d /%m /%y    %H:%M:%S    %Y ",t))
+print("formatted time:",time.strftime("%c    %H:%M:%S    %Y ",t)) 
+print("......................................")
+
---
+++ CO3_python_rashid/graphics/circle.py
@@ -0,0 +1,4 @@
+def areac(r):
+    print("area of circle is",3.14*r*r)
+    print("perimeter of circle is",2*3.14*r) 
+ 
