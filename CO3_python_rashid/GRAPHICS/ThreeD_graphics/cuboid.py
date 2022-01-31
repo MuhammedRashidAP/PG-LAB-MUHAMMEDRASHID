d221b32603ac52d6d694c86f4b5df703b31e60bc
MuhammedRashidAP <93854829+MuhammedRashidAP@users.noreply.github.com>
2022-01-31 19:18:26 +0530

    Add files via upload

---
+++ CO3_python_rashid/GRAPHICS/ThreeD_graphics/cuboid.py
@@ -0,0 +1,4 @@
+def area(l,b,h):
+    print("Area of Cuboid: ",(2*l*b)+(2*l*h)+(2*h*b))
+def perimeter(l,b,h):
+    print("Perimeter of Cuboid: ", 4*(l+b+h))
---
+++ CO3_python_rashid/GRAPHICS/ThreeD_graphics/sphere.py
@@ -0,0 +1,4 @@
+def area(r):
+    print("Surface Area of Sphere: ",4*3.14*r*r)
+def volume(r):
+    print("Volume of Sphere: ",(4/3)*3.14*r*r*r)
